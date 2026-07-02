485288b969c15c411c2086fa8866c84b8a9660c5
Baek Jihoon <133375485+Chuseok22@users.noreply.github.com>
2026-07-02 18:01:07 +0900

    Swagger_초기설정 : feat : JPA Auditing 기능 활성화 https://github.com/eodaego/eodaego-BE/issues/1

--- src/main/java/com/chuseok22/eodaegoserver/EodaegoServerApplication.java
+++ src/main/java/com/chuseok22/eodaegoserver/EodaegoServerApplication.java
@@ -2,8 +2,10 @@ package com.chuseok22.eodaegoserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class EodaegoServerApplication {
 
   public static void main(String[] args) {
