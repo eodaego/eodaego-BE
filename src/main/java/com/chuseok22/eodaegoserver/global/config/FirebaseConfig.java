335a304edae090b608cb13006b8673ba4c789da3
Baek Jihoon <133375485+Chuseok22@users.noreply.github.com>
2026-07-05 16:04:33 +0900

    Spring_Security_기본_설정_및_로그인_기능_구현 : feat : FirebaseConfig 서비스 계정 키 로딩 방식을 FileInputStream에서 ClassPathResource로 변경 https://github.com/eodaego/eodaego-BE/issues/3

--- src/main/java/com/chuseok22/eodaegoserver/global/config/FirebaseConfig.java
+++ src/main/java/com/chuseok22/eodaegoserver/global/config/FirebaseConfig.java
@@ -5,12 +5,13 @@ import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
 
 @Configuration
 @RequiredArgsConstructor
@@ -24,7 +25,8 @@ public class FirebaseConfig {
     if (!FirebaseApp.getApps().isEmpty()) {
       return FirebaseApp.getInstance();
     }
-    try (FileInputStream serviceAccount = new FileInputStream(firebaseProperties.serviceAccountKeyPath())) {
+    try (InputStream serviceAccount = new ClassPathResource(
+        firebaseProperties.serviceAccountKeyPath()).getInputStream()) {
       FirebaseOptions options = FirebaseOptions.builder()
           .setCredentials(GoogleCredentials.fromStream(serviceAccount))
           .build();
